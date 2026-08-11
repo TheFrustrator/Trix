0ee7350488b25ccbe53c471d6301f97f4a560bda
TheFrustrator <sudip.bhunia23@tnu.in>
2026-08-11 20:44:51 +0530

    v1.0.0.19

--- backend/routes/patientRoute.js
+++ backend/routes/patientRoute.js
@@ -2,12 +2,12 @@ import express from "express";
 import userAuth from "../middleware/userAuth.js";
 import {
   getPatientAccessRequests,
-  updateRequestStatus,
+  updateAccessRequestStatus,
 } from "../controllers/patientController.js";
 
 const patientRouter = express.Router();
 
 patientRouter.get("/access-requests", userAuth, getPatientAccessRequests);
-patientRouter.post("/update-request-status", userAuth, updateRequestStatus);
+patientRouter.post("/update-request-status", userAuth, updateAccessRequestStatus);
 
 export default patientRouter;
